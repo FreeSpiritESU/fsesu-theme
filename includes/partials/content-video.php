81ba543f563869f84d9c7dc16d9312ca08ed6307
Richard Perry <richard@perrymail.me.uk>
2014-08-08 14:36:58 +0000

    Removed unnecessary additional files from class based theme attempt
    Not all the files from before this orphaned branch was created had been deleted
    before for various reasons. I have now removed them for the sake of simplicity

--- includes/partials/content-video.php
+++
@@ -1,7 +0,0 @@
-            <?php get_template_part( 'includes/partials/post', 'header' ); ?> 
-                
-                <section class='entry-media'>
-                    <?php the_content(); ?>
-                </section><!-- .entry-media -->
-                
-	        </article><!-- #post-<?php the_ID(); ?> -->
